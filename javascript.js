ee24c6a4e94f217301094d884171c5d9ba6d71e5
oyshe886 <oyshe241-15-886@diu.edu.bd>
2025-08-29 20:12:12 +0600

    call button function

--- javascript.js
+++ javascript.js
@@ -29,3 +29,39 @@ for (const btn of copyBtns) {
     alert(`নম্বর কপি হয়েছেঃ ${number}`);
   });
 }
+
+
+// call button
+const callBtns = document.querySelectorAll("#btn-call");
+for (const btn of callBtns) {
+  btn.addEventListener("click", function (e) {
+    const number =
+      e.target.closest("#btn-call").parentNode.parentNode.children[1]
+        .children[2].innerText;    
+    const name =
+      e.target.closest("#btn-call").parentNode.parentNode.children[1]
+        .children[0].innerText;  
+    const nameEnglish = e.target.closest("#btn-call").parentNode.parentNode.children[1]
+        .children[1].innerText
+    if (coinCount < 20) {
+      alert("❌ আপনার পর্যাপ্ত কয়েন নেই! কল করতে কমপক্ষে ২০ কয়েন লাগবে।");
+    } else {
+      coinCount -= 20;
+      getElement("coin-count").innerText = coinCount;
+      alert(`📞 Calling ${nameEnglish} ${number}...`);
+      const time = new Date().toLocaleTimeString();
+      const historyContainer = getElement("history-id");
+      const history = document.createElement("div");
+      history.innerHTML = `
+      <div class="bg-gray-100 flex items-center justify-between p-3 rounded-lg">
+            <div>
+              <p>${name}</p>
+              <p class="text-gray-600">${number}</p>
+            </div>
+            <p>${time}</p>
+          </div>
+      `;
+      historyContainer.append(history);
+    }
+  });
+}
